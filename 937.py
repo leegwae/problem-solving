5afc1f334feb1be57a31fd13eb4d82975cabbd94
leegwae <leegwae@gmail.com>
2021-07-16 00:29:32 +0900

    Add 937.py

---
+++ 937.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letter_logs = []
+        digit_logs = []
+        
+        for log in logs:
+            parsed = log.split()
+            # 숫자 로그라면
+            if parsed[1].isdecimal():
+                digit_logs.append(log)
+            else: # 문자 로그라면
+                letter_logs.append(log)
+                
+        letters = sorted(letter_logs, key=lambda x : (x.split()[1:], x.split()[0]))
+        
+        return letters + digit_logs
