b4de93b20f5d05793a76ad55e98e6412c20a5708
leegwae <leegwae@gmail.com>
2022-04-19 22:02:39 +0900

    iteration과 recursion 사용 함수 분리

--- prefix_sum/구간_합_구하기_4.py
+++ prefix_sum/구간_합_구하기_4.py
@@ -1,11 +1,5 @@
+import sys
 from typing import List
-
-n, m = map(int, input().split())
-numbers = [0] + list(map(int, input().split()))
-scopes = []
-
-for i in range(m):
-	scopes.append(list(map(int, input().split())))
 
 
 def fun_get_prefix_sum(arr: List[int], pos: int) -> List[int]:
@@ -18,11 +12,24 @@ def fun_get_prefix_sum(arr: List[int], pos: int) -> List[int]:
 	return fun_get_prefix_sum(arr, pos + 1)
 
 
-def get_prefix_sum(arr: List[int]) -> None:
+def get_prefix_sum_recursion(arr: List[int]) -> None:
 	fun_get_prefix_sum(arr, 0)
 
 
-get_prefix_sum(numbers)
+def get_prefix_sum(arr: List[int]) -> None:
+	for i in range(1, len(arr)):
+		arr[i] += arr[i-1]
 
-for [a, b] in scopes:
-	print(numbers[b] - numbers[a - 1])
+
+if __name__ == '__main__':
+	n, m = map(int, sys.stdin.readline().split())
+	numbers = [0] + list(map(int, sys.stdin.readline().split()))
+	scopes = []
+
+	for i in range(m):
+		scopes.append(list(map(int, sys.stdin.readline().split())))
+
+	get_prefix_sum(numbers)
+
+	for [a, b] in scopes:
+		print(numbers[b] - numbers[a - 1])
