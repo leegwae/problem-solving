1f959e68a6cd378c10969e10f4ec5362654b6ce6
leegwae <leegwae@gmail.com>
2022-06-15 23:11:45 +0900

    Add brute-forcing code for LIS

--- LIS/가장_긴_증가하는_부분_수열.py
+++ LIS/가장_긴_증가하는_부분_수열.py
@@ -4,7 +4,7 @@ sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
 
-def LIS_1():
+def bottom_up():
 	dp = [1] * N
 
 	def f(n):
@@ -15,51 +15,70 @@ def LIS_1():
 
 		return max(dp)
 
-	print(f(N))
+	return f(N)
 
 
-def LIS_2():
+def top_down():
 	AA = [0] + A
-	dp = [-1] * (N + 1)
+	NN = len(AA)
+	dp = [-1] * NN
 
 	def f(cur):
 		if dp[cur] != -1:
 			return dp[cur]
 
 		result = 0
-		for nxt in range(cur + 1, N + 1):
+		for nxt in range(cur + 1, NN):
 			if AA[nxt] > AA[cur]:
 				result = max(result, f(nxt) + 1)
 		dp[cur] = result
 
 		return result
 
-	print(f(0))
+	return f(0)
 
 
-def LIS_3():
-	dp = [[-1] * N for _ in range(N)]
+# def LIS_3():
+# 	dp = [[-1] * N for _ in range(N)]
+#
+# 	def f(prev, cur):
+# 		if cur == N:
+# 			return 0
+#
+# 		if dp[prev][cur] != -1:
+# 			return dp[prev][cur]
+#
+# 		result = f(prev, cur + 1)
+# 		if prev == -1 or A[prev] < A[cur]:
+# 			result = max(result, f(cur, cur + 1) + 1)
+# 		dp[prev][cur] = result
+#
+# 		return result
+#
+# 	print(f(-1, 0))
 
-	def f(prev, cur):
-		if cur == N:
+
+def brute_forcing():
+	def f(arr):
+		if len(arr) == 0:
 			return 0
 
-		if dp[prev][cur] != -1:
-			return dp[prev][cur]
-
-		result = f(prev, cur + 1)
-		if prev == -1 or A[prev] < A[cur]:
-			result = max(result, f(cur, cur + 1) + 1)
-		dp[prev][cur] = result
-
+		result = 1
+		for cur in range(0, len(arr)):
+			s = []
+			for nxt in range(cur + 1, len(arr)):
+				if arr[cur] < arr[nxt]:
+					s.append(arr[nxt])
+			result = max(result, f(s) + 1)
 		return result
 
-	print(f(-1, 0))
+	return f(A)
 
 
 if __name__ == '__main__':
 	N = int(input())
 	A = list(map(int, input().split()))
-	LIS_1()
-	# LIS_2()
-	# LIS_3()
+	# answer = brute_forcing()
+	answer = bottom_up()
+	# answer = top_down()
+	print(answer)
